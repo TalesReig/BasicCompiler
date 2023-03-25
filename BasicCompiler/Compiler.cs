7d008f8545767f8d35060a44ce8276ef7bb8aaf9
Tales Reig Cordova <talesreig03@gmail.com>
2023-03-25 20:08:28 -0300

    Add project files.

---
+++ BasicCompiler/Compiler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCompiler
+{
+    internal class Compiler
+    {
+        List<string> palavrasReservadas = new List<string>();
+        List<string> ListaDeErros = new List<string>();
+        List<string> lexemas = new List<string>();
+        List<string> tokens = new List<string>();
+ 
+        public Compiler()
+        {
+            preencherPalavrasReservadas();
+        }
+
+        public void compilerLine(string line)
+        {
+
+        }
+
+        private void preencherPalavrasReservadas()
+        {
+            palavrasReservadas.Add("public");
+            palavrasReservadas.Add("void");
+            palavrasReservadas.Add("int");
+            palavrasReservadas.Add("float");
+            palavrasReservadas.Add("char");
+            palavrasReservadas.Add("boolean");
+            palavrasReservadas.Add("if");
+            palavrasReservadas.Add("else");
+            palavrasReservadas.Add("for");
+            palavrasReservadas.Add("while");
+            palavrasReservadas.Add("scanf");
+            palavrasReservadas.Add("println");
+            palavrasReservadas.Add("return");
+        }
+    }
+}
---
+++ BasicCompiler/Program.cs
@@ -0,0 +1,27 @@
+﻿using BasicCompiler;
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            Compiler compiladorTabajara = new Compiler();
+            string filePath = "C:\\MeusDocumentos\\arquivo.txt";
+            StreamReader sr = new StreamReader(filePath);
+            string linha;
+
+            while ((linha = sr.ReadLine()) != null)
+            {
+                compiladorTabajara.compilerLine(linha);
+                Console.WriteLine(linha);
+            }
+            sr.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error reading file: {ex.Message}");
+        }
+    }
+}
